053a2f1b5458feadf41aabe325034035906f25cf
Ruth Naomi <imoruth746@gmail.com>
2024-09-28 03:56:49 +0300

    Add files via upload

---
+++ src/context/AppReducer.js
@@ -0,0 +1,31 @@
+const appReducer =(state, action) => {
+    switch (action.type) {
+      case "GET_TRANSACTIONS":
+        return {
+          ...state,
+          loading: false,
+          transactions: action.payload
+        };
+      case "DELETE_TRANSACTION":
+        return {
+          ...state,
+          transactions: state.transactions.filter(
+            transaction => transaction._id !== action.payload
+          )
+        };
+      case "ADD_TRANSACTION":
+        return {
+          ...state,
+          transactions: [...state.transactions, action.payload]
+        };
+  
+      case "TRANSACTION_ERROR":
+        return {
+          ...state,
+          transactions: [action.payload, ...state.transactions]
+        };
+      default:
+        return state;
+    }
+  };
+  export default appReducer;
